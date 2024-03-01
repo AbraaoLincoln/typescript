f72c7f4b4802624ace85fcfaa082332c18be8349
abraao <abraao@pop-os.localdomain>
2024-03-01 09:52:56 -0300

    firs commit

---
+++ src/type_annotation/type_annoyaation.ts
@@ -0,0 +1,18 @@
+// basic types
+const name: string = 'fulano'
+const age: number = 18
+const isAdult: boolean = true
+const symbol: symbol = Symbol('anything')
+
+//Arrays
+const ticketsNumbers: Array<number> = [1, 2, 3]
+const ticketsNumbers2: number[] = [3,4,5]
+
+//Objest
+const person: {name: string, age:number, isAdult?: boolen} = {
+    name,
+    age
+}
+
+//Functions
+const sum: (x: number, y: number) => number = (x, y) => x + y
